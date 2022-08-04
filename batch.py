43b4e24698dd8c86437c1a387dd8402fae3cd63f
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2022-08-04 04:12:01 -0500

    Setting up framework for scripts.

---
+++ batch.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+
+"""
+
+import json
+import argparse
+
+
+def parse_args():
+    """
+
+    """
+    pass
+
+def intialize_parser():
+    """
+
+    """
+    pass
+
+
+def main():
+    """
+
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
